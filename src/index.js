134fee4a06b9c74c6505f304bd0473d52f64d828
francesco-strazzullo <me@francescostrazzullo.info>
2016-11-19 15:33:21 +0100

    added UI events to set values

--- src/index.js
+++ src/index.js
@@ -32,12 +32,39 @@ const DEFINITION = {
     age: Number
 };
 
-const INITITAL_TARGET = {};
+const INITITAL_TARGET = {
+    name: 'Francesco'
+};
 
 const person = typed({
     target:INITITAL_TARGET,
     definition:DEFINITION}
 );
 
-person.name = 'Pippo';
-person.surname = 0;
+const printToDOM = () => {
+    document.getElementById('data').innerText = JSON.stringify(person,undefined,2);
+    document.getElementById('error').innerText = "";
+};
+
+const setSurname = (surname) => {
+    person.surname = surname;
+    printToDOM();
+};
+
+const setAge = (age) => {
+    person.age = age;
+    printToDOM();
+};
+
+const onerror = (event) => {
+    document.getElementById('error').innerText = event;
+    return false;
+};
+
+Object.assign(window,{
+    setAge,
+    setSurname,
+    onerror
+});
+
+printToDOM();
